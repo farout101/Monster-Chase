76ca8357d0a0b9b7be9e949616b891c5eed90570
Phyo Zaw Linn <phyozawlinn1852020@gmail.com>
2024-06-23 13:18:45 +0630

    jumping

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -17,8 +17,11 @@ public class Player : MonoBehaviour
     private Animator anim;
     private SpriteRenderer sr;
 
+    private bool isGrounded = true;  // Flag to indicate if player is grounded
+    private readonly string GROUND_TAG = "Ground";
+
     private readonly string WALK_ANIMATION = "walk";
-    // Start is called before the first frame update
+    private bool isJumping = false;  // Flag to indicate jump input
 
     private void Awake()
     {
@@ -32,12 +35,18 @@ public class Player : MonoBehaviour
 
     }
 
-    // Update is called once per frame
     void Update()
     {
         playerMoveKeyboard();
-
         AnimatePlayer();
+        CheckJump();  // Check for jump input
+    }
+
+    private void FixedUpdate() {
+        if (isJumping) {
+            PlayerJump();
+            isJumping = false;  // Reset jump flag
+        }
     }
 
     void playerMoveKeyboard()
@@ -45,19 +54,15 @@ public class Player : MonoBehaviour
         movementX = Input.GetAxisRaw("Horizontal"); // GetAxis can give the float values!
 
         transform.position += moveForce * Time.deltaTime * new Vector3(movementX, 0, 0);
-
-        // Debug.Log("Movement value : " + transform.position);
     }
 
     void AnimatePlayer()
     {
-        // going right
         if (movementX > 0)
         {
             sr.flipX = false;
             anim.SetBool(WALK_ANIMATION, true);
         }
-        // goiding left
         else if (movementX < 0)
         {
             sr.flipX = true;
@@ -66,6 +71,23 @@ public class Player : MonoBehaviour
         else
         {
             anim.SetBool(WALK_ANIMATION, false);
+        }
+    }
+
+    void CheckJump() {
+        if (Input.GetButtonDown("Jump") && isGrounded) {
+            isGrounded = false ;  // Set flag to indicate player is no longer grounded
+            isJumping = true;  // Set jump flag when button is pressed
+        }
+    }
+  
+    void PlayerJump() {
+        myBody.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision){
+        if (collision.gameObject.CompareTag(GROUND_TAG)) {
+            isGrounded = true;
         }
     }
 }
